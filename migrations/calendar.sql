6d9c10b8de41cfdb06981ff66d52f86afb3f1b3b
Mario <marioermondo1@gmail.com>
2026-04-20 12:37:18 -0700

    migraciones: mover a carpeta unica migrations/

--- migrations/calendar.sql
+++ migrations/calendar.sql
@@ -1,2 +1,5 @@
+-- tablas del modulo de calendario
+-- ejecutar en la DB nootra
+
 # Agregar columna is_done para marcar eventos completados, 17/04/2026
 ALTER TABLE tasks ADD COLUMN is_done TINYINT(1) DEFAULT 0 AFTER all_day;
