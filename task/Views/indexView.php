cace4c34774e18c3bf83308afeeff854fa425184
Jesus Rafael Contreras Galvez <leafar.555cg@gmail.com>
2026-04-22 00:06:40 -0700

    /add(task): agregar componentes minimos en index

---
+++ task/Views/indexView.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"></script>
+    <script src="../js/includes/lightMode.js" defer></script>
+    <script src="../js/includes/toast.js"></script>
+    <link rel="stylesheet" href="../css/includes/toast.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="../css/includes/sidebar.css">
+    <link rel="stylesheet" href="../css/includes/lightMode.css">
+    <title>Document</title>
+</head>
+<body>
+    <script>lucide.createIcons({attrs: {'stroke-width': 1.6, stroke: 'currentColor'}});</script>
+    <script src="../js/includes/sidebar.js"></script>
+</body>
+</html>
