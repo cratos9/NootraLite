32591d1bea21bf19f2d22c0f7e9f3b7e07e7878d
Jesus Rafael Contreras Galvez <leafar.555cg@gmail.com>
2026-04-22 00:39:07 -0700

    add/(task): agregar estilos al index.

--- task/Views/indexView.php
+++ task/Views/indexView.php
@@ -8,12 +8,18 @@
     <script src="../js/includes/toast.js"></script>
     <link rel="stylesheet" href="../css/includes/toast.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/task/index.css">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="../css/includes/sidebar.css">
     <link rel="stylesheet" href="../css/includes/lightMode.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <h1>Mis tareas</h1>
+    </header>
+    <main>
+    </main> 
     <script>lucide.createIcons({attrs: {'stroke-width': 1.6, stroke: 'currentColor'}});</script>
     <script src="../js/includes/sidebar.js"></script>
 </body>
